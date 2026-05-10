b38dac2fbf6dc99eae11c0104d413fe6dbec5286
vancebg <vance_@abv.bg>
2026-05-10 12:33:19 +0300

    Chapter 7

---
+++ Chapter 7/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int accumulate(int x) {
+
+	static int s_value = 0;
+	s_value += x;
+	return s_value;
+}
+
+int main()
+{
+	std::cout << accumulate(4) << '\n'; // prints 4
+	std::cout << accumulate(3) << '\n'; // prints 7
+	std::cout << accumulate(2) << '\n'; // prints 9
+	std::cout << accumulate(1) << '\n'; // prints 10
+
+	return 0;
+}
